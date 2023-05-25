d10d59ec0b696dce70b8f5b03bfda332a58cd214
JoonHyeongHong <jshwa0429@gmail.com>
2023-05-25 19:16:12 +0900

    server 추가

---
+++ server/App.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const mongoose = require('mongoose');
+
+onst uri =
+  "mongodb+srv://jshwa0429:asrai%237931@cluster0.9njhpor.mongodb.net/test";
+
+async function connect() {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected to mongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+connect();
+
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const user = new User({
+  username: "jack",
+  password: "12345",
+});
+
+user
+  .save()
+  .then(() => {
+    console.log("New user has been saved to the database successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server Listening on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.post("/", (req, res) => {
+  const { username, password } = req.body;
+  const user = new User({ username, password });
+  user
+    .save()
+    .then(() => res.send("User saved to the database"))
+    .catch((error) => res.status(500).send(error));
+});
